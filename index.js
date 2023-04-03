43cfa8ba0e0eee921448906571bca9e59862e392
Chandler Farley <chandlerfarley@live.com>
2023-04-03 15:56:34 -0500

    updated lab

--- index.js
+++ index.js
@@ -3,7 +3,7 @@
 const num1 = 2, num2 = 31;
 const multiply = num1 * num2;
 
-const random = Math.floor(Math.random() * 10);
+const random = Math.floor(Math.random() * 10) +1;
 
 const num3 = 24, num4 = 5;
 const mod = num3 % num4;
