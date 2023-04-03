7307248db9c2f041f57bd9007503c30faf9771c1
Chandler Farley <chandlerfarley@live.com>
2023-04-03 15:50:35 -0500

    complete lab

--- index.js
+++ index.js
@@ -1,2 +1,11 @@
 
 // Write your code here
+const num1 = 2, num2 = 31;
+const multiply = num1 * num2;
+
+const random = Math.floor(Math.random() * 10);
+
+const num3 = 24, num4 = 5;
+const mod = num3 % num4;
+
+const max = Math.max(1, 13, 20);
